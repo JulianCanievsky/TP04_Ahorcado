5591d1303af3e283bdfde229654774626da06e19
JULIÁN MARIO CANIEVSKY <49550988@ort>
2025-05-14 08:41:13 -0300

    primerComitTp04

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP04_Canievsky.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
